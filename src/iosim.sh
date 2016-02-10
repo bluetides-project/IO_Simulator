1782709042fe435c67a2b7f05941e6db3ed9ff3d
mscheucher <scheucher@cmu.edu>
2016-02-09 22:00:39 -0500

    PBS File for COMA

---
+++ src/iosim.sh
@@ -0,0 +1,55 @@
+#!/bin/sh -f
+
+#  21cmFASTrun.sh
+#  
+#
+#  Created by Markus Scheucher on 08/02/16.
+#
+
+#PBS -N IO-SIMULATOR
+#PBS -o stdout_file
+#PBS -e stderr_file
+#PBS -j oe
+#PBS -l walltime=00:59:00
+#PBS -q bigmem 
+#   #PBS -l mem=512mb
+#PBS -l nodes=1:ppn=4
+
+cd $PBS_O_WORKDIR
+
+##########################################
+#                                        #
+#   Output some useful job information.  #
+#                                        #
+##########################################
+
+echo ------------------------------------------------------
+echo -n 'Job is running on node '; cat $PBS_NODEFILE
+echo ------------------------------------------------------
+echo PBS: qsub is running on $PBS_O_HOST
+echo PBS: originating queue is $PBS_O_QUEUE
+echo PBS: executing queue is $PBS_QUEUE
+echo PBS: working directory is $PBS_O_WORKDIR
+echo PBS: execution mode is $PBS_ENVIRONMENT
+echo PBS: job identifier is $PBS_JOBID
+echo PBS: job name is $PBS_JOBNAME
+echo PBS: node file is $PBS_NODEFILE
+echo PBS: current home directory is $PBS_O_HOME
+echo PBS: PATH = $PBS_O_PATH
+echo ------------------------------------------------------
+echo ' '
+echo ' '
+
+############################################################
+#                                                          #
+#    Execute the run.  Do not run in the background.       #
+#                                                          #
+############################################################
+
+
+mpiexec ./iosim -N 81 -n 4 -s 81000 -m create TestFile-81-4-81000
+
+exit
+#
+#
+#
